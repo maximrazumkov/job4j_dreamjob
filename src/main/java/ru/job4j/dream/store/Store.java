4ad757f889ff76b5fa1bec064a8b661be1850c8c
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-05-02 21:09:52 +0300

    1. CandidateSerlvet. Создание кандидата.[#279368]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -11,6 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Store {
 
     private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
 
     private static final Store INST = new Store();
 
@@ -46,5 +47,10 @@ public class Store {
     public void save(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 }
