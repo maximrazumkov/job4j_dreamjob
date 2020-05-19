476a39cf83fe1c303959533f66e0941cba45a189
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-05-19 13:54:08 +0300

    4. Ajax Jquery[#279343]

---
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -0,0 +1,24 @@
+package ru.job4j.dream.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GreetingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        resp.setHeader("Access-Control-Max-Age", "3600");
+        resp.setHeader("Access-Control-Allow-Headers", "*");
+        String name = req.getParameter("name");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println("Nice to meet you, " + name);
+        writer.flush();
+    }
+}
